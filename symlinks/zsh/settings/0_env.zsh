6ac3e241f62404a918125888d2595be8b2efcab2
Martin Domke <mail@martindomke.net>
2017-04-13 09:40:33 +0200

    Configure pyenv root directory.

--- symlinks/zsh/settings/0_env.zsh
+++ symlinks/zsh/settings/0_env.zsh
@@ -6,5 +6,6 @@ export VISUAL="nvim"
 export GOPATH="$HOME/Source/play/go"
 export HSPATH="$HOME/.cabal/bin"
 export TEXBINDIR="/Library/TeX/texbin"
+export PYENV_ROOT="/usr/local/var/pyenv"
 
-export PATH="$HOME/.bin:$PATH:$HSPATH:$GOPATH/bin:$TEXBINDIR:$HOME/kube-solo/bin"
+export PATH="$HOME/.bin:$PATH:$HSPATH:$GOPATH/bin:$TEXBINDIR:$HOME/kube-solo/bin:$PYENV_ROOT/bin"

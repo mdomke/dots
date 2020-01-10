92d19db9dab3f21c60f513f5c046664720f0d957
Martin Domke <mail@martindomke.net>
2020-01-10 11:23:02 +0100

    Add pyenv initialization

--- symlinks/zsh/settings/pyenv.zsh
+++ symlinks/zsh/settings/pyenv.zsh
@@ -1,3 +1,7 @@
 export WORKON_HOME=${HOME}/.virtualenvs
 export PROJECT_HOME=${HOME}/Source
 source /usr/local/bin/virtualenvwrapper_lazy.sh
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi

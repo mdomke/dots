4176da8e339413a777fd68c81f3821b86e72cf7f
Martin Domke <martin.domke@finleap.com>
2021-03-15 11:42:12 +0100

    Use relative path for home dir

--- symlinks/zsh/settings/gcloud.zsh
+++ symlinks/zsh/settings/gcloud.zsh
@@ -1,8 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '~/.bin/google-cloud-sdk/path.zsh.inc' ]; then . '~/.bin/google-cloud-sdk/path.zsh.inc'; fi
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/mdomke/.bin/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/mdomke/.bin/google-cloud-sdk/path.zsh.inc'; fi
-
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/mdomke/.bin/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/mdomke/.bin/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '~/.bin/google-cloud-sdk/completion.zsh.inc' ]; then . '~/.bin/google-cloud-sdk/completion.zsh.inc'; fi

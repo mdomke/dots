6cd62e67174e7940b007f64564d2c7fa56c60d52
Martin Domke <martin.domke@finleap.com>
2023-06-29 16:23:06 +0200

    chore: Configure kube-ps1 for good measure

--- home/dot_zsettings/kube-ps1.zsh
+++ home/dot_zsettings/kube-ps1.zsh
@@ -1,3 +1,9 @@
 export KUBE_PS1_SYMBOL_ENABLE=false
 export KUBE_PS1_PREFIX=''
 export KUBE_PS1_SUFFIX=''
+
+get_cluster_short() {
+  echo "$1" | sd "fcloud-|-(oncall|default|shared)" ""
+}
+
+export KUBE_PS1_CLUSTER_FUNCTION=get_cluster_short

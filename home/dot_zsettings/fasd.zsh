ef0182cd3801692e95d025b98b25bf4f38b977f3
Martin Domke <martin.domke@finleap.com>
2023-06-29 16:15:00 +0200

    Don't use the default fasd aliases

--- home/dot_zsettings/fasd.zsh
+++ home/dot_zsettings/fasd.zsh
@@ -1,1 +1,14 @@
-source <(fasd --init auto)
+source <(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)
+
+fasd_cd() {
+	local fasd_ret="$(fasd -d "$@")"
+	if [[ -d "$fasd_ret" ]]
+	then
+		cd "$fasd_ret"
+	else
+		print "$fasd_ret"
+	fi
+}
+
+alias z="fasd_cd -d"
+alias zz="fasd_cd -d -i"

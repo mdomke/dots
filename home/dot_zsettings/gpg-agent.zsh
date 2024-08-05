b75abbd778632a0ab56ba79a5aab28a356473862
Martin Domke <mail@martindomke.net>
2024-08-05 12:49:36 +0200

    Update config

--- home/dot_zsettings/gpg-agent.zsh
+++ home/dot_zsettings/gpg-agent.zsh
@@ -1,5 +1,5 @@
-export GPG_TTY=$(tty)
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-fi
+# export GPG_TTY=$(tty)
+# unset SSH_AGENT_PID
+# if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+#   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+# fi
